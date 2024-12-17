e3fe0504b7f4935cd90ca472aa5f8d99eb7c5074
Fiston <karekezifiston30@gmail.com>
2024-12-17 18:26:33 +0200

    a

--- frontend/src/components/DiscoverSermon/DiscoverSermon.jsx
+++ frontend/src/components/DiscoverSermon/DiscoverSermon.jsx
@@ -1,19 +1,70 @@
-import React from 'react'
-import './DiscoverSermon.css'
+import React, { useState } from 'react';
+import './DiscoverSermon.css';
 
 const DiscoverSermon = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const sermons = [
+    {
+      id: 1,
+      title: 'Sermon 1: Faith',
+      url: 'https://www.youtube.com/embed/DKJblcRRR3I?si=lFrxt6lBIMzYtCWB',
+    },
+    {
+      id: 2,
+      title: 'Sermon 2: Hope',
+      url: 'https://www.youtube.com/embed/OaHNIG-f3K4?si=W_MD5TRmtXrDcEn2',
+    },
+    {
+      id: 3,
+      title: 'Sermon 3: Love',
+      url: 'https://www.youtube.com/embed/DKJblcRRR3I?si=lFrxt6lBIMzYtCWB',
+    },
+    {
+      id: 4,
+      title: 'Sermon 4: Patience',
+      url: 'https://www.youtube.com/embed/OaHNIG-f3K4?si=W_MD5TRmtXrDcEn2',
+    },
+  ];
+
+  const filteredSermons = sermons.filter((sermon) =>
+    sermon.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className='discover-sermon'>
-    <div className='discover-title'>
+    <div className="discover-sermon">
+      <div className="discover-title">
         <h2>Watch Sermon</h2>
         <hr />
-    </div>
-    <div>
-        
-    </div>
-      
-    </div>
-  )
-}
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search sermons..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
 
-export default DiscoverSermon
+      <div className="container">
+        {filteredSermons.length > 0 ? (
+          filteredSermons.map((sermon) => (
+            <div className="sermon-item" key={sermon.id}>
+              <iframe
+                src={sermon.url}
+                title={sermon.title}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+              <h2>{sermon.title}</h2>
+            </div>
+            
+          ))
+        ) : (
+          <p className="no-results">No sermons found</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default DiscoverSermon;
